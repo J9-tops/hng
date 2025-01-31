8e22d2ede86a496462a368cfb289d8968b46d1ca
J9-tops <j9.tops@gmail.com>
2025-01-31 11:50:32 +0100

    Submission

--- stage0/task2/index.js
+++ stage0/task2/index.js
@@ -7,27 +7,4 @@ function updateUTCTime() {
   } UTC`;
 }
 
-// Update UTC time when the page loads
-setInterval(() => {
-  updateUTCTime();
-}, 1000);
-
-// function updateSpacerHeight() {
-//   const absoluteElement = document.querySelector(".profile-description");
-//   const spacer = document.querySelector(".spacer");
-
-//   if (absoluteElement && spacer) {
-//     spacer.style.height = `${absoluteElement.offsetHeight}px`;
-//   }
-// }
-
-// // Observe changes in the absolute element's size
-// const observer = new ResizeObserver(updateSpacerHeight);
-// const absoluteElement = document.querySelector(".absolute-box");
-
-// if (absoluteElement) {
-//   observer.observe(absoluteElement);
-// }
-
-// // Initial update in case the element is already rendered
-// updateSpacerHeight();
+updateUTCTime();
